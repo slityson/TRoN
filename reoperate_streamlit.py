0e3291069b3040cea6516ce390d9c6e3042fcda2
slityson <159221105+slityson@users.noreply.github.com>
2024-02-07 14:58:34 -0500

    digraph antics

--- reoperate_streamlit.py
+++ reoperate_streamlit.py
@@ -317,9 +317,10 @@ def run_streamlit(graph_json_data_path,table_json_data_path):
     else:
         # Code for filtering dataframe and generating network
         st.session_state.df_select = st.session_state.df_interact.loc[st.session_state.df_interact['source'].isin(selected_nodes) | st.session_state.df_interact['target'].isin(selected_nodes)]
-        st.session_state.df_select = st.session_state.df_select.reset_index(drop=True)
+        # st.session_state.df_select = st.session_state.df_select.reset_index(drop=True)
         # Create networkx graph object from pandas dataframe
         G = nx.from_pandas_edgelist(st.session_state.df_select, 'source', 'target')
+        st.dataframe(st.session_state.df_interact)
         # Create pyvis network
         HtmlFile = generate_pyvis_network(G,400)
         # Add button to download network as html
